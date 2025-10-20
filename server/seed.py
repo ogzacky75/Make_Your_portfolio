d9f41c41373beaacab660dcc53d22eeba426440c
brenda njoki <ngunjiribrenda5@gmail.com>
2025-10-20 14:56:35 +0300

    Adds Template model with name, image anwith name,image and description

--- server/seed.py
+++ server/seed.py
@@ -27,3 +27,36 @@ def seed_users():
             email = 'liamBrown@gmail.com'
         )
         user4.set_password('liampassword67')
+
+        db.session.add_all([user1, user2, user3, user4])
+        db.session.commit()
+        print("Users seeded successfully.")
+
+def seed_templates():
+    with app.app_context():
+        template1 = Template(
+            name = 'Modern Portifolio',
+            image = '',
+            description = 'A sleek and modern portfolio template perfect for showcasing your work.'
+        )
+
+        template2 = Template(
+            name = 'Creative Portfolio',
+            image = '',
+            description = 'A vibrant and creative portfolio template designed to highlight your unique style.'
+        )
+
+        template3 = Template(
+            name = 'Professional Portfolio',
+            image = '',
+            description = 'A clean and professional portfolio template ideal for business presentations'
+        )
+
+        template4 = Template(
+            name = 'Developer Portfolio',
+            image = '',
+            description = 'A teachnical and structured portfolio template tailored for developers. '
+        )
+
+
+
