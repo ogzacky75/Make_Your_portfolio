e7c8e8212094e700cb1d75ca011628973c181acf
Mathu025 <mathualex72@gmail.com>
2025-10-22 12:47:53 +0300

    Create Login Page and add routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './Pages/SignUp.jsx';
+import LogIn from './Pages/LogIn.jsx';
 
 function App() {
   return (
       <Routes>
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
   );
 }
