88633340d8bdcd4f7ec73db41241cb50217139bd
brenda njoki <ngunjiribrenda5@gmail.com>
2025-10-26 09:13:44 +0300

    Created a SideBar data for the links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import Sidebar from "./components/Sidebar.jsx"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./components/HomePage.jsx";
---
+++ client/src/components/SideBarData.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const  SideBarData  = [
+   {
+    title: "Home",
+    icon: <HomeIcon/>,
+    link: "/home"
+
+   },
+   {
+    title: "Template",
+    icon: <TemplateIcon/>,
+    link: "/template"
+   }
+
+]
+
+
+
+
