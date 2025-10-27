1e230781df96f0154dc9966e0456265b8dd3eef6
brenda njoki <ngunjiribrenda5@gmail.com>
2025-10-27 23:44:33 +0300

    Updates the app.jsx for routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from "react";
-import Sidebar from "./components/Sidebar.jsx"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
-import HomePage from "./components/HomePage.jsx";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.jsx';
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/homepage" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+       
       </Routes>
     </Router>
   );
