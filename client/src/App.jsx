698cdd32579327a73c9bac0bcdf042957d8f699d
Mathu025 <mathualex72@gmail.com>
2025-10-28 14:34:49 +0300

    Create temporary landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './Pages/SignUp.jsx';
 import LogIn from './Pages/LogIn.jsx';
+import Landing from './Pages/Landing.jsx';  
 
 function App() {
   return (
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/landing" element={<Landing />} />
       </Routes>
   );
 }
