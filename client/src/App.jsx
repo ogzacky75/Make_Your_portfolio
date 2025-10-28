4d8962e342115cc0300fc5004b0bfa2a55003c8c
brenda njoki <ngunjiribrenda5@gmail.com>
2025-10-28 19:57:54 +0300

    Updates templates and favorites routes on App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-       
+        <Route path="/" element={<Sidebar />} />
+        <Route path="/templates" element={<TemplatesPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
